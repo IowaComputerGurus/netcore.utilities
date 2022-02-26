d97f0a5343d9dfd5ee96b6e25b582179169e4f28
Mitchel Sellers <msellers@iowacomputergurus.com>
2022-02-26 00:27:49 -0600

    Added key creator exampe

---
+++ src/EncryptionKeyGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ICG.NetCore.Utilities;
+using Microsoft.Extensions.Options;
+
+Console.WriteLine("Welcome to ICG NetCore Utilities Encryption Key Generator");
+var serviceInstance = new AesEncryptionService(new OptionsWrapper<AesEncryptionServiceOptions>(new AesEncryptionServiceOptions()));
+var newKey = serviceInstance.GenerateEncryptionSecrets();
+Console.WriteLine($"Key: {newKey.Key}");
+Console.WriteLine($"IV: {newKey.IV}");
+Console.WriteLine("Press any key to exit");
+Console.ReadLine();
